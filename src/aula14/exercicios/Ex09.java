6401df1ee815d6a537a2f79a805894f096996db1
CaMaschion <cm.maschion@gmail.com>
2021-01-18 21:01:22 -0300

    continuação exercícios parte I

--- src/aula14/exercicios/Ex09.java
+++ src/aula14/exercicios/Ex09.java
@@ -1,8 +1,33 @@
 package aula14.exercicios;
 
+import java.util.Scanner;
+
 public class Ex09 {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        int numero, numero2, numero3;
+
+        System.out.println("Insira um valor: ");
+        numero = scan.nextInt();
+
+        System.out.println("Insira outro valor: ");
+        numero2 = scan.nextInt();
+
+        System.out.println("Insira mais um valor: ");
+        numero3 = scan.nextInt();
+
+
+        if (numero <= numero2 && numero <= numero3 && numero2 <= numero3) {
+            System.out.println(numero3 + " - " + numero2 + " - " + numero);
+
+        } else if (numero2 <= numero && numero <= numero3 && numero3 <= numero2) {
+            System.out.println(numero2 + " - " + numero3 + " - " + numero);
+
+        } else if (numero3 <= numero && numero3 <= numero2 && numero <= numero2) {
+            System.out.println(numero + " - " + numero2 + " - " + numero3);
+        }
     }
 }
--- src/aula14/exercicios/Ex10.java
+++ src/aula14/exercicios/Ex10.java
@@ -1,4 +1,26 @@
 package aula14.exercicios;
 
+import java.util.Scanner;
+
 public class Ex10 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Em que turno você estuda? Entre com valores M - V ou N");
+        String turno = scan.next();
+
+        if (turno.equalsIgnoreCase("M")) {
+            System.out.println("Bom dia");
+        } else if (turno.equalsIgnoreCase("V")) {
+            System.out.println("Bom tarde");
+        } else if (turno.equalsIgnoreCase("N")) {
+            System.out.println("Boa noite");
+        } else {
+            System.out.println("Valor inválido");
+        }
+    }
+
 }
